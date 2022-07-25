bab0e03a5d931e1dd8d6d61e05248c0204e47b3d
Chelly izraelov <hlly.lov@gmail.com>
2022-07-25 16:05:52 +0300

    ...

--- src/n0160/easy/intersectionofTwoLinkedLists/ListNode.java
+++ src/n0160/easy/intersectionofTwoLinkedLists/ListNode.java
@@ -1,12 +1,18 @@
 package n0160.easy.intersectionofTwoLinkedLists;
 
 public class ListNode {
-
     int val;
     ListNode next;
 
-    ListNode(int x) {
-        val = x;
-        next = null;
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
