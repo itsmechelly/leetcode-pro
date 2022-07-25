66b9c5074047b49154c0ba24cfd0fe5e1f9cfc78
Chelly izraelov <hlly.lov@gmail.com>
2022-07-25 18:21:19 +0300

    ...

---
+++ src/n0003/medium/longestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,32 @@
+package n0003.medium.longestSubstringWithoutRepeatingCharacters;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
+        int max = 0;
+        int j = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (hashMap.containsKey(s.charAt(i))) {
+                j = Math.max(j, hashMap.get(s.charAt(i)) + 1);
+            }
+            hashMap.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcabc"));
+    }
+}
