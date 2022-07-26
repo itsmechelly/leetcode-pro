c34860cd5d61070d8230c76dc65a6454bad2e3a2
Chelly izraelov <hlly.lov@gmail.com>
2022-07-26 17:23:06 +0300

    ...

--- src/n567/medium/permutationInString/Solution.java
+++ src/n567/medium/permutationInString/Solution.java
@@ -1,2 +1,31 @@
-package n567.medium.permutationInString;public class Solution {
+package n567.medium.permutationInString;
+
+import java.util.Arrays;
+
+class Solution {
+
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        s1 = sort(s1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length())))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String sort(String string) {
+        char[] sortedArray = string.toCharArray();
+        Arrays.sort(sortedArray);
+        return new String(sortedArray);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.checkInclusion("ab", "eidbaooo"));
+        System.out.println(Solution.checkInclusion("ab", "eidboaoo"));
+    }
 }
--- src/n567/medium/permutationInString/SolutionWithHashMap.java
+++ src/n567/medium/permutationInString/SolutionWithHashMap.java
@@ -1,31 +1,43 @@
 package n567.medium.permutationInString;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
-class Solution {
+class SolutionWithHashMap {
 
     public static boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length()) {
             return false;
         }
 
-        s1 = sort(s1);
+        HashMap<Character, Integer> s1map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s1.length(); i++) {
+            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            if (s1.equals(sort(s2.substring(i, i + s1.length())))) {
+            HashMap<Character, Integer> s2map = new HashMap<Character, Integer>();
+            for (int j = 0; j < s1.length(); j++) {
+                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
+            }
+            if (matches(s1map, s2map)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static String sort(String string) {
-        char[] sortedArray = string.toCharArray();
-        Arrays.sort(sortedArray);
-        return new String(sortedArray);
+    public static boolean matches(HashMap<Character, Integer> s1map, HashMap<Character, Integer> s2map) {
+        for (char key : s1map.keySet()) {
+            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(Solution.checkInclusion("ab", "eidbaooo"));
-        System.out.println(Solution.checkInclusion("ab", "eidboaoo"));
+        System.out.println(SolutionWithHashMap.checkInclusion("ab", "eidbaooo"));
+        System.out.println(SolutionWithHashMap.checkInclusion("ab", "eidboaoo"));
     }
 }
